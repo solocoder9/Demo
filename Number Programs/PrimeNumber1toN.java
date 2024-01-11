22e7fd8084c2d2114f4b4b89bd5a5340cba5fffe
solocoder9 <solocoder9@gmail.com>
2024-01-11 22:12:53 +0530

    Number Programs

---
+++ Number Programs/PrimeNumber1toN.java
@@ -0,0 +1,30 @@
+// Program to print prime numbers between 1 to N
+
+import java.util.Scanner;
+
+public class PrimeNumber1toN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        for(int i = 2; i <= n; i++) {
+            int factorCount = 0;
+
+            // check the number is prime or not
+            for(int j = 2; j < i; j++) {
+                if(i % j == 0) {
+                    factorCount++;
+                }                
+            }
+
+            if(factorCount == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }    
+}
