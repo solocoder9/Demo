f12a36b50b8ca4b37499a442e64a98454353b88e
solocoder9 <solocoder9@gmail.com>
2024-01-14 23:06:44 +0530

    Patterns

---
+++ Pattern Programs/Star Pattern/Pattern4.java
@@ -0,0 +1,28 @@
+/*
+
+    *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+
+ */
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= 2*n-1; i++) {
+            
+        }
+    }
+}
